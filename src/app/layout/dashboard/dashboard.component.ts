137e4f6924e4e8fdfa1a41ace450fce75bf9f65a
Rain <shunchitrain@gmail.com>
2024-07-31 16:37:59 +0800

    add display student data in table

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,27 +1,39 @@
 import { AfterViewInit, Component, OnInit, } from '@angular/core';
 import { StudentApiService } from 'src/app/services/student-api.service';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { StudentType } from 'src/app/types/student-type';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  providers: [ ],
-  imports: [ ],
+  providers: [],
+  imports: [NgxDatatableModule],
   standalone: true
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
-  constructor(private studentService:StudentApiService ) { }
-   
+  constructor(private studentService: StudentApiService) { }
+  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Age' }, { name: 'City' }, { name: 'Province' }, { prop: 'hope_salary' }, { name: 'salary' }, { name: 'group' }];
+  studentList!: StudentType
+  loadingIndicator = true;
   ngOnInit() {
-this.getStudentList()
+    this.getStudentList()
   }
   ngAfterViewInit() {
 
   }
-  getStudentList(){
-    this.studentService.getStudentListAPI().subscribe((res)=>
-      console.log('res',res)
-  )
-}
+  getStudentList() {
+    this.studentService.getStudentListAPI().subscribe(
+      (next) => {
+        this.studentList = next.data
+      },
+      (error) => {
+        console.error(error);
+      },
+      () => {
+        console.log('completed');
+      }
+    )
+  }
 }
